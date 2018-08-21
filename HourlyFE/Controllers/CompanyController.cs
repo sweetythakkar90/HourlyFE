84ff9d43388c1d4ac69bb9596fa69f2ceeaa4587
sweetythakkar90 <sweetythakkar90@gmail.com>
2018-08-21 16:26:20 +1000

    get and add employee details using api from front end

--- HourlyFE/Controllers/CompanyController.cs
+++ HourlyFE/Controllers/CompanyController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using HourlyFE.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +13,38 @@ namespace HourlyFE.Controllers
 {
     public class CompanyController : Controller
     {
+        
+        string Baseurl = "http://localhost:37716/";
         // GET: Company
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            List<EmployeeModel> EmployeeInfo = new List<EmployeeModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+
+                // Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllComapnies using HttpClient 
+                HttpResponseMessage Res = await client.GetAsync("api/employees");
+
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api 
+                    var EmployeeResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Company list  
+                    EmployeeInfo = JsonConvert.DeserializeObject<List<EmployeeModel>>(EmployeeResponse);
+                }
+
+                //returning the company list to view  
+                return View(EmployeeInfo);
+            }
         }
     }
 }
---
+++ HourlyFE/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HourlyFE.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string gender { get; set; }
+        public int salary { get; set; }
+    }
+}
