26fe9f5694be4178289579bda2b534556a49cba7
sweetythakkar90 <sweetythakkar90@gmail.com>
2018-08-21 16:58:33 +1000

    Changes for calling post method from controller - error - nto working fully

--- HourlyFE/Controllers/CompanyController.cs
+++ HourlyFE/Controllers/CompanyController.cs
@@ -46,5 +46,37 @@ namespace HourlyFE.Controllers
                 return View(EmployeeInfo);
             }
         }
+        public async Task<ActionResult> Register(EmployeeModel Employee)
+        {
+            List<EmployeeModel> EmployeeInfo = new List<EmployeeModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+
+                var employee = new StringContent(JsonConvert.SerializeObject(Employee));
+
+                // Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                //Sending request to find web api REST service resource GetAllComapnies using HttpClient 
+                HttpResponseMessage Res = await client.PostAsync("api/employees", employee);
+
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api 
+                    var EmployeeResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Company list  
+                    EmployeeInfo = JsonConvert.DeserializeObject<List<EmployeeModel>>(EmployeeResponse);
+                }
+
+                //returning the company list to view  
+                return View("Index");
+            }
+        }
     }
 }
